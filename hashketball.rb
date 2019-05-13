23235fa8779729040f77bfb68216aead37497905
Jasmine Chen <jasmine1226@gmail.com>
2019-05-13 06:28:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,17 @@
 # Write your code here!
 
-
-
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "Brooklyn Nets"
+      colors: "",
+      players: {
+        alan_anderson:
+      }
+      }, 
+    away: {}, 
+  }
+end
 
 
 
